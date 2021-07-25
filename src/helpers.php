ec8dc3f0a0c841d6272720450433f652ce5fe4b9
ramirophp <ramiro_php@protonmail.com>
2021-07-25 12:56:25 -0600

    validando funciones helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,17 +1,23 @@
 <?php 
 
-//doctype
-function doctype () {
-    $ramiro_php = new Rmo\ramiro_php();
-    $ramiro_php->setModificador(0);
-    $ramiro_php->setElemento();
-    echo $ramiro_php->getElemento();
+if (!function_exists('doctype')) {
+//<!DOCTYPE html>
+    function doctype () {
+        $ramiro_php = new Rmo\ramiro_php();
+        $ramiro_php->setModificador(0);
+        $ramiro_php->setElemento();
+        echo $ramiro_php->getElemento();
+    }
 }
 
-function upper($value) {
-    return Rmo\Format::upperText($value);
+if (!function_exists('doctype')) {
+    function upper($value) {
+        return Rmo\Format::upperText($value);
+    }
 }
 
-function lower($value) {
-    return Rmo\Format::lowerText($value);
+if (!function_exists('doctype')) {
+    function lower($value) {
+        return Rmo\Format::lowerText($value);
+    }
 }
