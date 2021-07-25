8be645eeb8cadd4f37597fc2b366c7a6509b7017
ramirophp <ramiro_php@protonmail.com>
2021-07-25 12:39:06 -0600

    iniciando con composer

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-#//Test 01 Crear el Doctype
+#//Test 01 Crear el Doctype Sin Composer
     //file
-    require_once './ramiro_php/ramiro_php.php';
+    #require_once './ramiro_php/ramiro_php.php';
     /*
         instancia/objeto//por default el constructor 
         toma los valores necesarios para crear el doctype
     */
-    $ramiro_php = new ramiro_php();
+    #$ramiro_php = new ramiro_php();
     //core metodos
     /*
         $ramiro_php->setEtiqueta(); //seteada por el constructor por primera vez
@@ -19,7 +19,18 @@
         $ramiro_php->getContenido();
         $ramiro_php->noJs();
     */
+    #$ramiro_php->setModificador(0);
+    #$ramiro_php->setElemento();
+    #echo $ramiro_php->getElemento();
+#
+
+require_once __DIR__ . '/vendor/autoload.php';
+#//Test 01 Crear el Doctype Con Composer
+    $ramiro_php = new Rmo\ramiro_php();
     $ramiro_php->setModificador(0);
     $ramiro_php->setElemento();
     echo $ramiro_php->getElemento();
 #
+
+//Test 01 clase Format
+echo Rmo\Format::upperText('hola');
---
+++ src/Format.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rmo;
+
+class Format {
+
+    public static function upperText ($value) {
+
+        return strtoupper($value);
+
+    }
+
+}
